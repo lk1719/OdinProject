cc03e2c66b357d0d7bb58bf0e78f5ccccb3643b9
lk1719 <lkendrick904@gmail.com>
2023-12-01 16:49:09 -0500

    updated files

---
+++ rockPaperScissorsProject/projectJS.js
@@ -0,0 +1,8 @@
+function getComputerChoice () {
+    let array = ['Rock','Paper','Scissors']
+    return array[Math.random()*array.length]
+}
+
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection === 'rock' && computerSelection === 'rock')
+}
